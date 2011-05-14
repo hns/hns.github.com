d53244028249cbba37730b0c65965c77678170ba
Hannes Wallnöfer <hannesw@gmail.com>
2011-05-15 01:13:25 +0200

    Automatically run jekyll when page is requested

--- _config.js
+++ _config.js
@@ -3,12 +3,16 @@
 
 var response = require("ringo/jsgi/response");
 var strings = require("ringo/utils/strings");
+var subprocess = require("ringo/subprocess");
 
 exports.app = function(req) {
     var path = req.pathInfo;
     if (strings.endsWith(path, "/")) path += "index.html";
     var resource = getResource("./_site" + path);
     if (resource.exists()) {
+        if (strings.endsWith(path, ".html")) {
+            subprocess.system("jekyll", {dir: module.directory});
+        }
         return response.static(resource);
     }
     return response.notFound(req.pathInfo);
