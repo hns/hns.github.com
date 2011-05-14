394f6b80067a109f6a6bc90d5b00635707a509f1
Hannes Wallnöfer <hannesw@gmail.com>
2011-05-14 18:01:56 +0200

    Update preview app to ringo master

--- _config.js
+++ _config.js
@@ -1,7 +1,15 @@
 #! /usr/bin/env ringo-web
-
 // adapter to preview site via ringo
 
-exports.static = [
-   ["/", "_site"]
-];
+var response = require("ringo/jsgi/response");
+var strings = require("ringo/utils/strings");
+
+exports.app = function(req) {
+    var path = req.pathInfo;
+    if (strings.endsWith(path, "/")) path += "index.html";
+    var resource = getResource("./_site" + path);
+    if (resource.exists()) {
+        return response.static(resource);
+    }
+    return response.notFound(req.pathInfo);
+}
